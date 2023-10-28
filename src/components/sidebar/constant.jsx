0520cfeba084faf387668423a4c1b1185a8e6e09
Taner Tun <t_tun@msn.com>
2023-10-28 19:47:15 +0300

    sidebar, item,item number styled

---
+++ src/components/sidebar/constant.jsx
@@ -0,0 +1,28 @@
+
+export const Items = [
+    {
+      id: 'Step1',
+      number:1,
+      subtitle: 'Step 1',
+      title: 'Your Info'
+    },
+    {
+      id: 'Step2',
+      number:2,
+      subtitle: 'Step 2',
+      title: 'Select Plan'
+    },
+    {
+      id: 'Step3',
+      number:3,
+      subtitle: 'Step 3',
+      title: 'Add-Ons'
+    },
+    {
+      id: 'Step4',
+      number:4,
+      subtitle: 'Step4',
+      title: 'Summary'
+    }
+  ]
+  
--- src/components/sidebar/styled.jsx
+++ src/components/sidebar/styled.jsx
@@ -1,8 +1,34 @@
 import styled from 'styled-components';
 
-export const Sidebar = styled.div``
-export const Item = styled.div``
-export const ItemNumber = styled.span``
+import SidebarBg from "../../assets/bg-sidebar-desktop.svg";
+
+export const Sidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  background-image: url(${SidebarBg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: var(--white);
+  padding: 24px;
+  width: 35%;
+  border-radius: 8px;
+`
+export const Item = styled.div`
+ display: flex;
+ align-items: center;
+ gap: 8px;
+`
+export const ItemNumber = styled.span`
+  width: 36px;
+  height: 36px;
+  border: 1px solid var(--white);
+  border-radius: 999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 export const ItemBody = styled.div``
 export const Subtitle = styled.h5``
 export const Title = styled.h2``
