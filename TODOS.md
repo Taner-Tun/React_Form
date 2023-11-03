019cd42563d59e62f6733099fae8e619f1e755b2
Taner Tun <t_tun@msn.com>
2023-11-03 20:06:40 +0200

    step3 fixed and styled

--- TODOS.md
+++ TODOS.md
@@ -5,7 +5,7 @@
 [*] Step Component
 [*] Step1 Component
 [*] Step2 Component
-[] Step3 Component
+[*] Step3 Component
 [] Collect Form Data
 [] Step4 Component
 
--- src/components/step-3/index.jsx
+++ src/components/step-3/index.jsx
@@ -1,12 +1,43 @@
+import { useState } from "react"
 import Step from "../step";
-
+import FormJSON from "../../../form.json";
 import * as S from "./styled";
 
+
+
+const { step3 } = FormJSON;
+
 function Step3(props) {
+  const [selectedAddons, setSelectedAddons] = useState([]);
+
+  const BillingType = "monthly";
+
+  const changeSelectedAddons = (checked, selectedAddon) => {
+    if (checked) {
+      setSelectedAddons([...selectedAddons, selectedAddon]);
+    } else {
+      setSelectedAddons(
+        selectedAddons.filter((addon) => addon.id !== selectedAddon.id)
+      );
+    }
+  };
+  console.log(selectedAddons);
+  
+  const checkSelected = (id) => selectedAddons.some((i) => i.id === id);
   return (
     <Step {...props}>
       <S.Step3>
-        Step3
+        {step3[BillingType].map(item =>
+          <S.Item key={item.id} isselected={checkSelected(item.id)}>
+          <S.Input type="checkbox" onChange={(e) => changeSelectedAddons(e.target.checked, item)}/>
+          <S.InputBody>
+            <S.Title>{item.title}</S.Title>
+            <S.Subtitle>{item.description}</S.Subtitle>
+          </S.InputBody>
+          <S.Price>{item.price}</S.Price>
+        </S.Item>
+        )
+     }
       </S.Step3>
     </Step>
   )
